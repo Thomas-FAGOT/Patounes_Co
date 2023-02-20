54a1fa94c96cceac341e4d79373b177cbe730cde
Thomas-FAGOT <thomasfagot@ynov.com>
2023-02-20 20:19:05 +0100

    ajout de l'env + nav bar V1

--- Patounes_Co/listings/views.py
+++ Patounes_Co/listings/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from listings.models import Band
 
-# Create your views here.
+def accueil(request):
+    bands = Band.objects.all()
+    return render(request, 'listings/accueil.html', {'bands': bands})
+
+
+def about(request):
+    return HttpResponse('<h1>A propos</1> <p>Nous adorons Patounes & Co</p>')
